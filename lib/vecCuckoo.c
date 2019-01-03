290129191b5a81ed275c1a37cb0849ed54cecaff
FlavScheidt <flavscheidr@gmail.com>
2019-01-03 16:05:04 -0200

    Starting new version of vectorized Cuckoo Join

--- lib/vecCuckoo.c
+++ lib/vecCuckoo.c
@@ -1,70 +1,56 @@
 #include "vecCuckoo.h"
 
-inline int vecCuckooInsert(uint32_t key)
-{
-	uint32_t index;
-	uint16_t hps = 0;
-
-	//Variables for the AVX funcs
-	int * base_addr;
-	int vIndex;
-	int scale;
-
-	//8 positions!!!!
-	if (vecCuckooLookUp(key) > 0)
-		return 0;
-
-	sprintf(str, "%d", key);
-	index = H1;
-
-	do
-	{
-		//Load the 8 positions
-		base_addr = &cuckoo[(index*8)-1];
-		vIndex = ;
-		scale = 1;
-
-		//Find a empty one
-
-		//Insert
-
-		//No empty position? Then HOP
-		hps++;
-	} while (hps < VCUCKOO_MAX_TRY);
-
 inline void vecCuckooGenerate(column_orders * c_orders)
 {
+	//Vectors and masks
+	__m256i loadMask; //Mask used to load new keys
+	__m256i keysVector; //Vector of keys
+	__m256i newKeysVector; //Auxiliary vector used to load new keys
+	__m256i permutationMask; //Mask to shuffle new and old keys
+	__m256i hashedVector; //Hashed keys
+	__m256i table1Mask; //Keys to be inserted on table1
+	__m256i table2Mask; //Keys to be inserted on table2
+
+	//Auxiliary Masks
+	__m256i mask_1 = _mm256_set1_epi32(1);
+	__m256i mask_0 = _mm256_set1_epi32(0);
+
+	//Initiate vectors and masks
+	loadMask, keysVector, newKeysVector, permutationMask, hashedVector, table1Mask, table2Mask = _mm256_cmpeq_epi32(mask_1, mask_1);
+
+	//Performance counter variables
 	clock_t init, end;
 
-	init = clock();
-	likwid_markerStartRegion("Initialization");
-	for (int i=0; i<VEC_SIZE; i++)
-		cuckoo[i] = 0;
-	likwid_markerStopRegion("Initialization");
-	end = clock();
-	printf("Initialization: %.f ms \n", ((double)(end - init) / (CLOCKS_PER_SEC / 1000)));
+	//Other variables
+	size_t tuples = 0;
 
-	init = clock();
-	likwid_markerStartRegion("Generation");
-	for (int i=0; i<tamOrders; i++)
+	//Start Occupation Mask
+	do 
 	{
-		switch (vecCuckooInsert(c_orders[i].O_CUSTKEY))
-		{
-			case 0:
-				printf("ERROR: Something went wrong while inserting the key %u on the Table\n", c_orders[i].O_CUSTKEY);
-				break;
-			case 1:
-				OCC++;
-				break;
-			case 2:
-				DUP++;
-				break;
-		}	
-	}
-	likwid_markerStopRegion("Generation");
-	end = clock();
-	printf("Vectorized Cuckoo Generation: %.f ms \n", ((double)(end - init) / (CLOCKS_PER_SEC / 1000)));
-	printf("%d keys, %d duplicated\n", OCC, DUP);
+		//Load the new items using the loadMask
+		newKeysVector = _mm256_maskload_epi32(&c_orders[i]->O_CUSTKEY, loadMask);
+
+		//Remove old items
+		keysVector = _mm256_andnot_si256(loadMask, keysVector);
+		table2Mask = _mm256_andnot_si256(loadMask, keysVector); //Every new key is inserted on table1, so they are not listed here
+
+		//Primary vector of keys receives new keys and table1 vector is set
+		keysVector = _mm256_or_si256(keysVector, newKeysVector);
+		table1Mask = _mm256_or_si256(table1Mask, mask_1);
+
+		//Hash the keys
+		hashedVector = H1; //First hash using mask with function1
+		hashedVector = H2; //Second hash using mask with function2
+
+		//Load the cuckoo table values - Check for zeros
+		newKeysVector = _mm256_i32gather_epi32(cuckoo, hashedVector, 4);
+
+
+
+
+
+	} while (tuples <= tamOrders-8)
+	
 }
 
 int vecCuckooJoin(column_customer * c_customer, column_orders * c_orders)
@@ -81,17 +67,16 @@ int vecCuckooJoin(column_customer * c_customer, column_orders * c_orders)
 
 	vecCuckooGenerate(c_orders, tamOrders);
 
-	init=clock();
-	likwid_markerStartRegion("Core");
-	for (int i=0; i<tamCustomer; i++)
-	{
-		if (vecCuckooLookUp(c_customer[i].C_CUSTKEY) == 0)
-			index++;
-	}
-	likwid_markerStopRegion("Core");
-	end=clock();
+	// init=clock();
+	// likwid_markerStartRegion("Core");
+	// for (int i=0; i<tamCustomer; i++)
+	// {
+	// 	if (vecCuckooLookUp(c_customer[i].C_CUSTKEY) == 0)
+	// 		index++;
+	// }
+	// likwid_markerStopRegion("Core");
+	// end=clock();
 
-	printf("Join core: %.f ms \n", ((double)(end - init) / (CLOCKS_PER_SEC / 1000)));
+	// printf("Join core: %.f ms \n", ((double)(end - init) / (CLOCKS_PER_SEC / 1000)));
 
-	return index;
-}
+    return index; }
