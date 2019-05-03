f20e1a7444702873aec2d624608f844a122f4d40
FlavScheidt <flavscheidr@gmail.com>
2019-05-03 18:26:28 -0300

    Fixing operation2

--- lib/vecCuckooFilter.c
+++ lib/vecCuckooFilter.c
@@ -207,7 +207,7 @@ inline void cViViDGenerateFilter(column_orders * c_orders)
 		temporaryPSVector2	= _mm256_cvtepi32_ps(integer256Vector);
 		temporaryPSVector3	= _mm256_div_ps(temporaryPSVector, temporaryPSVector2);
 
-		temporaryVector 	= _mm256_cvtps_epi32(temporaryPSVector3); //fp/5
+		temporaryVector 	= _mm256_round_ps(temporaryPSVector3, _MM_FROUND_TO_NEG_INF); //fp/5
 		temporaryVector 	= _mm256_mullo_epi32(temporaryVector, integer256Vector); // *5
 		positionVector 		= _mm256_mask_sub_epi32(positionVector, loadMask, fingerprintVector, temporaryVector); //-fp
 
